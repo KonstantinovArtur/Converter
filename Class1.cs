d9bddf0ab0c07b9dd77724543bd4efdbce722d91
KonstantinovArtur <akonstantinov538@gmail.com>
2023-10-30 18:46:20 +0300

    Конвертер

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Текстовый_конвертер
+{
+    internal class Book
+    {
+        public string Name;
+        public string Genre;
+        public int Num_Pages;
+
+    }
+    public class Book1
+    {
+        public string Name;
+        public string Genre;
+        public int Num_Pages;
+
+    }
+
+}
